1184d0d18552ec9c844699db3204e846f07d03f2
Yakov <ya.sharygin@onlystudio.org>
2023-10-02 18:08:06 +0500

    Настроил url для robots.txt

--- vysotsky_pro/urls.py
+++ vysotsky_pro/urls.py
@@ -19,12 +19,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
+from django.views.generic import TemplateView
 
 from landing.views import FrontendTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', FrontendTemplateView.as_view(), name='home'),
+    path('robots.txt', TemplateView.as_view(
+        template_name='robots.txt', content_type='text/plain'
+    ), name='robots'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
